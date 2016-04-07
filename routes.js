972a5fd9d25d84254adeca65f500d90ff2b6ea41
jayvachon <jaylvachon@gmail.com>
2016-04-07 12:53:10 -0400

    unregister host, request room list

--- routes.js
+++ routes.js
@@ -6,6 +6,9 @@ var rooms = require('./service/rooms');
 exports = module.exports = function (app) {
 
 	app.get('/registerHost/:name/:address', rooms.registerHost);
+	app.get('/unregisterHost/:id', rooms.unregisterHost);
+	app.get('/roomList', rooms.requestRoomList);
+	app.get('/reset', rooms.reset);
 
 	// deprecate
 	app.get('/addHost/:hostName/:hostIp', hosts.addHost);
--- service/rooms.js
+++ service/rooms.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var async = require('async');
+var _ = require('underscore');
 
 var rooms = {
 
 	registerHost: function (req, res) {
 
 		var outcome = {};
-		var name = req.params.hostName;
-		var address = req.params.hostAddress;
+		var name = req.params.name;
+		var address = req.params.address;
 
 		// Get the Client model associated with the host (if one exists)
 		var getClient = function (cb) {
@@ -16,6 +17,7 @@ var rooms = {
 				if (err) {
 					return cb(err, null);
 				}
+				console.log("FOUND: " + client);
 				outcome.client = client;
 				cb(null, 'done');
 			});
@@ -31,6 +33,7 @@ var rooms = {
 					if (err) {
 						return cb(err, null);
 					}
+					console.log("CREATED: " + client);
 					outcome.client = client;
 					cb(null, 'done');
 				});
@@ -75,6 +78,38 @@ var rooms = {
 		}
 
 		async.waterfall([getClient, createClient, getRoom, createRoom], asyncFinally);
+	},
+
+	unregisterHost: function (req, res) {
+
+		// Removes the room with the given id
+		req.app.db.models.Room.findByIdAndRemove(req.params.id, function(err) {
+			if (err) {
+				res.status(200).json({ result: 'error', details: err });
+			}
+			res.status(200).json({ result: 'success' });
+		});
+	},
+
+	requestRoomList: function (req, res) {
+
+		// Returns a list of all the rooms. List includes room ids and host data
+		req.app.db.models.Room.find({}).populate('host').exec(function(err, rooms) {
+			if (err) {
+				res.status(200).json({ result: 'error', details: err });
+			}
+			res.status(200).json(_.map(rooms, function(x) { return { roomId: x._id, host: x.host }; }));
+		});
+	},
+
+	reset: function (req, res) {
+		req.app.db.models.Room.remove({}, function(err) {
+			if (err) return err;
+			req.app.db.models.Client.remove({}, function(err) {
+				if (err) return err;
+				res.status(200).json({ result: "reset" });
+			});
+		});
 	}
 };
 
